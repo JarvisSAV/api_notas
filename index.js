f46f804c4b475e3dd981d295c26a7035978f3a3a
Silvino Aguiar <2121100431@soy.utj.edu.mx>
2023-06-23 00:02:48 -0600

    response de notas con content-type:json

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-console.log("Hola mundo desde node js")
+import http from 'http'
+
+let notes = [
+    {
+        "id": 1,
+        "content": "",
+        "date": "2023-05-30T17:30:31.098Z",
+        "important": true
+    },
+    {
+        "id": 2,
+        "content": "Tengo que estudiar las clases del FullStack Bootcamp",
+        "date": "2023-05-30T18:39:34.091Z",
+        "important": false
+    },
+    {
+        "id": 3,
+        "content": "Repasar los retos de JS de midudev",
+        "date": "2023-05-30T19:20:14.298Z",
+        "important": true
+    }
+]
+
+const app = http.createServer((_request, _response) => {
+    _response.writeHead(200, { 'Content-Type': 'application/json' })
+    _response.end(JSON.stringify(notes))
+    // _resp.end('Hello World')
+})
+
+const PORT = 4000
+app.listen(PORT)
+console.log(`Server runnin on port ${PORT}`)
